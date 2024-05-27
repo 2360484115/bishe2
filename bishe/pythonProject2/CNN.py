5b4e7688ddae167b25d2d2466a65fc81515d2f88
cxp <163701406+2360484115@users.noreply.github.com>
2024-05-28 02:59:23 +0800

    Update CNN.py

--- bishe/pythonProject2/CNN.py
+++ bishe/pythonProject2/CNN.py
@@ -7,8 +7,17 @@ from tensorflow.keras.layers import Dense, Conv1D, Flatten, Dropout, MaxPooling1
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.layers import Input
 import joblib
+from scipy import stats
+
 # 数据加载
-data_path = 'MHEALTHDATASET/mHealth_subject1.log'
+data1_path = 'MHEALTHDATASET/mHealth_subject1.log'
+data2_path = 'MHEALTHDATASET/mHealth_subject2.log'
+data3_path = 'MHEALTHDATASET/mHealth_subject3.log'
+data4_path = 'MHEALTHDATASET/mHealth_subject4.log'
+data5_path = 'MHEALTHDATASET/mHealth_subject5.log'
+data6_path = 'MHEALTHDATASET/mHealth_subject6.log'
+data7_path = 'MHEALTHDATASET/mHealth_subject7.log'
+data8_path = 'MHEALTHDATASET/mHealth_subject8.log'
 columns = [
     'acc_chest_x', 'acc_chest_y', 'acc_chest_z',  # 胸部加速度
     'acc_ankle_l_x', 'acc_ankle_l_y', 'acc_ankle_l_z',  # 左脚踝加速度
@@ -21,29 +30,72 @@ columns = [
 ]
 
 # 加载数据
-data = pd.read_csv(data_path, delimiter=r'\s+', header=None,
-                   usecols=[0, 1, 2, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23],
-                   names=columns)
-
+data1 = pd.read_csv(data1_path, delimiter=r'\s+', header=None, names=columns)
+data2 = pd.read_csv(data2_path, delimiter=r'\s+', header=None, names=columns)
+data3 = pd.read_csv(data3_path, delimiter=r'\s+', header=None, names=columns)
+data4 = pd.read_csv(data4_path, delimiter=r'\s+', header=None, names=columns)
+data5 = pd.read_csv(data5_path, delimiter=r'\s+', header=None, names=columns)
+data6 = pd.read_csv(data6_path, delimiter=r'\s+', header=None, names=columns)
+data7 = pd.read_csv(data7_path, delimiter=r'\s+', header=None, names=columns)
+data8 = pd.read_csv(data8_path, delimiter=r'\s+', header=None, names=columns)
+data = pd.concat([data1, data2, data3, data4, data5, data6])
+data_test = pd.concat([data7, data8])
 # 数据预处理
 X = data.drop('activity_label', axis=1)
-y = data['activity_label'].values  # 调整标签值以适应 to_categorical，从0开始编号
+y = data['activity_label'].values
+X_test = data_test.drop('activity_label', axis=1)
+y_test = data_test['activity_label'].values
 
+# print(X.shape, y.shape, X_test.shape, y_test.shape)
 # 标准化
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
+X_test_scaled = scaler.fit_transform(X_test)
 joblib.dump(scaler, 'scaler.pkl')
+print(X_scaled.shape, y.shape, X_test_scaled.shape, y_test.shape)
 
-# 划分数据集
-X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.3, random_state=42)
+
+def windowz(data, size, step):
+    start = 0
+    while start < len(data):
+        yield start, start + size
+        start += step
+
+
+def segment_opp(inputs, targets, window_size, step):
+    segments = np.zeros(((len(inputs) - window_size) // step + 1, window_size, 21))
+    labels = np.zeros(((len(targets) - window_size) // step + 1))
+    i_segment = 0
+    i_label = 0
+    for (start, end) in windowz(inputs, window_size, step):
+        if len(inputs[start:end]) == window_size:
+            m = stats.mode(y[start:end])
+            segments[i_segment] = X_scaled[start:end]
+            labels[i_label] = m[0]
+            i_label += 1
+            i_segment += 1
+    return segments, labels
+
+
+# 定义窗口大小和步长
+window_size = 50
+step = 25
+train_x, train_y = segment_opp(X_scaled, y, window_size, step)
+test_x, test_y = segment_opp(X_test_scaled, y_test, window_size, step)
+print("Shape of segmented train inputs: {}".format(train_x.shape))
+print("Shape of segmented train labels: {}".format(train_y.shape))
+print("Shape of segmented test inputs:  {}".format(test_x.shape))
+print("Shape of segmented test labels:  {}".format(test_y.shape))
+
+
 
 # 将标签转换为分类格式
-y_train_categorical = to_categorical(y_train)
-y_test_categorical = to_categorical(y_test)
+y_train_categorical = to_categorical(train_y)
+y_test_categorical = to_categorical(test_y)
 
 # 模型定义
 model = Sequential([
-    Input(shape=(X_train.shape[1], 1)),  # 使用Input层指定输入形状
+    Input(shape=(train_x.shape[1], train_x.shape[2])),  # 输入形状
     Conv1D(filters=64, kernel_size=3, activation='relu'),
     MaxPooling1D(pool_size=2),
     Dropout(0.5),
@@ -58,57 +110,10 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 # 模型摘要
 model.summary()
 
-# 数据扩展维度以适应模型输入 (batch, steps, channels)
-X_train_expanded = np.expand_dims(X_train, axis=2)
-X_test_expanded = np.expand_dims(X_test, axis=2)
-
 # 训练模型
-model.fit(X_train_expanded, y_train_categorical, epochs=10, batch_size=64, verbose=2)
-model.save('model.h5')
+model.fit(train_x, y_train_categorical, epochs=200, batch_size=64, verbose=2)
+
 # 评估模型
-loss, accuracy = model.evaluate(X_test_expanded, y_test_categorical, verbose=0)
+loss, accuracy = model.evaluate(test_x, y_test_categorical, verbose=0)
 print("测试准确率: {:.2f}%".format(accuracy * 100))
 print("测试损失值: {:.4f}".format(loss))
-# 新观测数据
-
-
-
-# new_observation = {
-#     'acc_chest_x': [-7.67],
-#     'acc_chest_y': [-0.33954],
-#     'acc_chest_z': [-1.999],
-#     'acc_ankle_l_x': [2.7197],
-#     'acc_ankle_l_y': [-8.9036],
-#     'acc_ankle_l_z': [-2.2409],
-#     'gyro_ankle_l_x': [-0.087199],
-#     'gyro_ankle_l_y': [0.090056],
-#     'gyro_ankle_l_z': [0.72495],  # 左脚踝角速度
-#     'mag_ankle_l_x': [9.6385],
-#     'mag_ankle_l_y': [3.3899],
-#     'mag_ankle_l_z': [-2.3912],  # 左脚踝磁力计
-#     'acc_arm_x': [-2.0764],
-#     'acc_arm_y': [-7.5394],
-#     'acc_arm_z': [1.0605],
-#     'gyro_arm_x': [-0.43725],
-#     'gyro_arm_y': [0.1848],
-#     'gyro_arm_z': [-0.25216],  # 右下臂角速度
-#     'mag_arm_x': [-2.3009],
-#     'mag_arm_y': [-14.654],
-#     'mag_arm_z': [56.779],  # 右下臂磁力计
-# }
-#
-# # 将新观测数据转换为DataFrame，使用相同的列名
-# new_data_df = pd.DataFrame(new_observation)
-#
-# # 标准化新数据
-# new_data_scaled = scaler.transform(new_data_df)
-#
-# # 为新数据增加一个维度以匹配CNN模型的输入要求
-# new_data_expanded = np.expand_dims(new_data_scaled, axis=2)
-#
-# # 使用模型进行预测
-# new_prediction = model.predict(new_data_expanded)
-# new_predicted_class = np.argmax(new_prediction, axis=1)
-#
-# # 打印预测结果
-# print("新数据的预测类别：", new_predicted_class)
